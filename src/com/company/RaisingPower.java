51e6710a0ff4c6b896f81ebbd8b9b7ba48b38480
Matthew Greco <mgreco2112@tutanota.com>
2022-11-14 14:47:15 -0500

    implemented BigDecimal solution, passed all but two cases but solved long overflow problem

--- src/com/company/RaisingPower.java
+++ src/com/company/RaisingPower.java
@@ -1,23 +1,52 @@
 package com.company;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class RaisingPower {
+//    public static int raisingPower(List<Integer> arr) {
+//        final long formula = (long) Math.pow(10, 9) + 7;
+//
+//        int result = 0;
+//        long modResult = Integer.MIN_VALUE;
+//
+//        for (int i = 0; i < arr.size() - 1; i++) {
+//            long power = (long) Math.pow(arr.get(i), arr.get(i + 1));
+//
+//            long powModFormula = power % formula;
+//
+//            System.out.println(powModFormula);
+//
+//            if (powModFormula > modResult) {
+//                modResult = powModFormula;
+//                result = i + 1;
+//            }
+//        }
+//
+//        return result;
+//
+//
+//
+//    }
+
     public static int raisingPower(List<Integer> arr) {
-        final long formula = (long) Math.pow(10, 9) + 7;
+        final BigDecimal bigFormula = new BigDecimal(Math.pow(10, 9) + 7);
 
         int result = 0;
-        long modResult = Integer.MIN_VALUE;
+        BigDecimal bigModResult = new BigDecimal(Integer.MIN_VALUE);
 
         for (int i = 0; i < arr.size() - 1; i++) {
-            long power = (long) Math.pow(arr.get(i), arr.get(i + 1));
 
-            long powModFormula = power % formula;
+            BigDecimal bigPower = new BigDecimal(Math.pow(arr.get(i), arr.get(i + 1)));
 
-            System.out.println(powModFormula);
 
-            if (powModFormula > modResult) {
-                modResult = powModFormula;
+            BigDecimal remainder = bigPower.remainder(bigFormula);
+
+            System.out.println(remainder);
+
+
+            if (remainder.compareTo(bigModResult) > 0) {
+                bigModResult = remainder;
                 result = i + 1;
             }
         }
@@ -26,5 +55,9 @@ public class RaisingPower {
 
 
 
+    }
+
+    public static void main(String[] args) {
+        raisingPower(List.of());
     }
 }
